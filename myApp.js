8378018aa074132cfc11017918db81a1213fa646
fedeft1980 <federicoftorres@gmail.com>
2025-09-03 08:08:58 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,1 +1,19 @@
+var express = require("express");
+var app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// 🚨 Este es el endpoint que valida el test
+app.get("/json", (req, res) => {
+  let data = { message: "Hello json" };
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    data.message = data.message.toUpperCase();
+  }
+  res.json(data);
+});
+
+module.exports = app;
